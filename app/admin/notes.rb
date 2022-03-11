dde94cab9d06ecb5a03443bc8bbd45b47d8e7626
Lincxx <bossco28@gmail.com>
2022-03-11 06:44:43 -0500

    Finished

--- app/admin/notes.rb
+++ app/admin/notes.rb
@@ -8,6 +8,29 @@ ActiveAdmin.register Note do
       f.input :title
       f.input :body,  as: :quill_editor
     end
+    f.actions
+  end
+
+
+  show title: 'Your note' do
+    h1 link_to note.title, admin_notes_path
+    h4 link_to note.subject.name, admin_subject_path(note.subject)
+    div(class: 'note-body') do 
+      raw note.body
+    end
+  end
+
+  index do
+    selectable_column 
+    column :subject
+    column 'Title' do |note|
+      link_to note.title, admin_note_path(note)
+    end
+    column 'Body' do |note|
+      raw note.body.truncate_words(25)
+    end
+    column :created_at
+    column :updated_at
   end
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
